56d8766bf60ada82c81dcf777544e3ead327ceb4
Hrólfur <hrolfurgylfa@users.noreply.github.com>
2024-12-31 17:13:34 +0000

    Removed unused import

--- src/scope.rs
+++ src/scope.rs
@@ -13,7 +13,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-use std::{borrow::Borrow, collections::HashMap, iter, sync::Arc};
+use std::{collections::HashMap, iter, sync::Arc};
 
 use crate::types::Type;
 
